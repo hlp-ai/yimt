86a1c7dc40c2f69e3d4800e53db4b46ece54571d
Xiaofeng Liu <liuxf@hust.edu.cn>
2024-02-17 15:47:55 +0800

    添加更多语言OCR例子。

--- EasyOCR/easyocr/ocr.py
+++ EasyOCR/easyocr/ocr.py
@@ -1,5 +1,25 @@
 import easyocr
 
-reader = easyocr.Reader(['ch_sim','en']) # this needs to run only once to load the model into memory
-result = reader.readtext('./examples/chinese.jpg')
+# reader = easyocr.Reader(['ch_sim','en']) # this needs to run only once to load the model into memory
+# result = reader.readtext('./examples/chinese.jpg')
+# print(result)
+
+# reader = easyocr.Reader(['ja','en']) # this needs to run only once to load the model into memory
+# result = reader.readtext('./examples/japanese.jpg')
+# print(result)
+
+# reader = easyocr.Reader(['ko','en']) # this needs to run only once to load the model into memory
+# result = reader.readtext('./examples/korean.png')
+# print(result)
+
+# reader = easyocr.Reader(['en']) # this needs to run only once to load the model into memory
+# result = reader.readtext('./examples/english.png')
+# print(result)
+
+# reader = easyocr.Reader(['th', 'en']) # this needs to run only once to load the model into memory
+# result = reader.readtext('./examples/thai.jpg')
+# print(result)
+
+reader = easyocr.Reader(['ar', 'en']) # this needs to run only once to load the model into memory
+result = reader.readtext('./examples/arab.png')
 print(result)
