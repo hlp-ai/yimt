77112aab28c4c3be85f9cdbbd956462b379573d8
Xiaofeng Liu <liuxf@hust.edu.cn>
2024-02-21 11:43:23 +0800

    改进代码缩进；删除无用imports。

--- vits/text/symbols.py
+++ vits/text/symbols.py
@@ -3,11 +3,10 @@
 '''
 Defines the set of symbols used in text input to the model.
 '''
-_pad        = '_'
+_pad = '_'
 _punctuation = ';:,.!?¡¿—…"«»“” '
 _letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
 _letters_ipa = "ɑɐɒæɓʙβɔɕçɗɖðʤəɘɚɛɜɝɞɟʄɡɠɢʛɦɧħɥʜɨɪʝɭɬɫɮʟɱɯɰŋɳɲɴøɵɸθœɶʘɹɺɾɻʀʁɽʂʃʈʧʉʊʋⱱʌɣɤʍχʎʏʑʐʒʔʡʕʢǀǁǂǃˈˌːˑʼʴʰʱʲʷˠˤ˞↓↑→↗↘'̩'ᵻ"
-
 
 # Export all symbols:
 symbols = [_pad] + list(_punctuation) + list(_letters) + list(_letters_ipa)
