0f7e38578dd770640e8718cfb4cbe23a525f167f
Xiaofeng Liu <liuxf@hust.edu.cn>
2024-03-06 20:17:41 +0800

    删除onmt2.3代码。

--- OpenNMT-py-2.3.0/onmt/decoders/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Module defining decoders."""
-from onmt.decoders.decoder import DecoderBase
-from onmt.decoders.transformer import TransformerDecoder, TransformerLMDecoder
-
-
-str2dec = {"transformer": TransformerDecoder,
-           "transformer_lm": TransformerLMDecoder}
-
-__all__ = ["DecoderBase", "TransformerDecoder", "str2dec", "TransformerLMDecoder"]
--- OpenNMT-py-2.3.0/onmt/encoders/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Module defining encoders."""
-from onmt.encoders.encoder import EncoderBase
-from onmt.encoders.transformer import TransformerEncoder
-from onmt.encoders.mean_encoder import MeanEncoder
-
-
-str2enc = {"transformer": TransformerEncoder, "mean": MeanEncoder}
-
-__all__ = ["EncoderBase", "TransformerEncoder", "MeanEncoder", "str2enc"]
