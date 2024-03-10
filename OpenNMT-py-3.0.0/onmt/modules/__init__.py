f7fd2af6d5520d351e59fd03f6ed5293ba52dfee
Xiaofeng Liu <liuxf@hust.edu.cn>
2024-03-10 10:59:15 +0800

    删除GlobalAttention。

--- OpenNMT-py-3.0.0/onmt/modules/__init__.py
+++ OpenNMT-py-3.0.0/onmt/modules/__init__.py
@@ -1,13 +1,11 @@
 """  Attention and normalization modules  """
 from onmt.modules.util_class import Elementwise
-from onmt.modules.global_attention import GlobalAttention
 from onmt.modules.copy_generator import CopyGenerator, CopyGeneratorLoss
 from onmt.modules.multi_headed_attn import MultiHeadedAttention
 from onmt.modules.embeddings import Embeddings, PositionalEncoding
 from onmt.modules.average_attn import AverageAttention
 
-__all__ = ["Elementwise",
-           "GlobalAttention", "CopyGenerator",
-           "CopyGeneratorLoss",
-           "MultiHeadedAttention", "Embeddings", "PositionalEncoding",
+__all__ = ["Elementwise", "CopyGenerator",
+           "CopyGeneratorLoss", "MultiHeadedAttention",
+           "Embeddings", "PositionalEncoding",
            "AverageAttention"]
