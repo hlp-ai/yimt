1eb589daf896cc287324c7ac5013d66f0fc50bdd
Xiaofeng Liu <liuxf@hust.edu.cn>
2024-05-06 16:44:28 +0800

    删除无用的模块。

--- mt/onmt/modules/__init__.py
+++ mt/onmt/modules/__init__.py
@@ -1,6 +1,5 @@
 """  Attention and normalization modules  """
 from onmt.modules.util_class import Elementwise
-from onmt.modules.gate import context_gate_factory, ContextGate
 from onmt.modules.global_attention import GlobalAttention
 from onmt.modules.copy_generator import CopyGenerator, CopyGeneratorLoss
 from onmt.modules.multi_headed_attn import MultiHeadedAttention
@@ -13,8 +12,6 @@ from onmt.modules.rmsnorm import RMSNorm
 
 __all__ = [
     "Elementwise",
-    "context_gate_factory",
-    "ContextGate",
     "GlobalAttention",
     "CopyGenerator",
     "CopyGeneratorLoss",
