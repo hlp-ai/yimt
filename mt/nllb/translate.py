4ec7101db25a3c998024822c040ec4ea0acafcaa
Xiaofeng Liu <liuxf@hust.edu.cn>
2024-05-10 16:59:29 +0800

    MT: 更新NLLB测试代码。

--- mt/nllb/translate.py
+++ mt/nllb/translate.py
@@ -1,3 +1,7 @@
+from torch.nn.utils.rnn import pad_sequence
+import torch
+
+from onmt.constants import DefaultTokens
 from onmt.opts import config_opts, translate_opts
 from onmt.translate.translator import build_translator
 from onmt.utils.parse import ArgumentParser
@@ -25,3 +29,35 @@ sp_model_src.Load(opt.src_subword_model)
 text = ["how are you?", "what are you doing now?"]
 segmented = sp_model_src.encode(text, out_type=str)
 print(segmented)
+
+src_prefix = "</s> eng_Latn"
+tgt_prefix = "zho_Hans"
+
+segmented_prefix = [src_prefix.split() + s for s in segmented]
+print(segmented_prefix)
+
+src_ids = [src_vocab(s) for s in segmented_prefix]
+print(src_ids)
+
+padidx = src_vocab[DefaultTokens.PAD]
+src_ids = [torch.LongTensor(ids) for ids in src_ids]
+src_ids_padded = pad_sequence(src_ids, batch_first=True,
+                             padding_value=padidx)
+print(src_ids_padded)
+
+tgt = [[tgt_prefix]] * len(text)
+print(tgt)
+tgt_ids = [src_vocab(s) for s in tgt]
+print(tgt_ids)
+tgt_ids = torch.LongTensor(tgt_ids)
+
+batch = {
+    "src": src_ids_padded,
+    "tgt": tgt_ids,
+    "srclen": torch.LongTensor([len(s) for s in src_ids])
+}
+
+print(batch)
+
+result = translator.translate_batch(batch, False)
+print(result)
