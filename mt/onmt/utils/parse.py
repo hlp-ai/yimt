cc6ae2f501ad1a235c15c99125dd40684165b9e7
Xiaofeng Liu <liuxf@hust.edu.cn>
2024-05-11 10:10:08 +0800

    MT: 删除SRU配置检查。

--- mt/onmt/utils/parse.py
+++ mt/onmt/utils/parse.py
@@ -272,9 +272,6 @@ class ArgumentParser(cfargparse.ArgumentParser, DataOptsCheckerMixin):
         same_size = model_opt.enc_hid_size == model_opt.dec_hid_size
         assert same_size, "The encoder and decoder rnns must be the same size for now"
 
-        assert (
-            model_opt.rnn_type != "SRU" or model_opt.gpu_ranks
-        ), "Using SRU requires -gpu_ranks set."
         if model_opt.share_embeddings:
             if model_opt.model_type != "text":
                 raise AssertionError("--share_embeddings requires --model_type text.")
