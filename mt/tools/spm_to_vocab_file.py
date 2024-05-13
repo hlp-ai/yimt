2f4e873156af3019dd5eee4bd8f697e2e31199d1
Xiaofeng Liu <liuxf@hust.edu.cn>
2024-05-13 16:29:05 +0800

    MT: 词典转换输出到文件中。

--- mt/tools/spm_to_vocab_file.py
+++ mt/tools/spm_to_vocab_file.py
@@ -1,8 +1,9 @@
 # converts a SentencePiece vocabulary to the format expected by dynamic data
 # (essentially converts float expected counts to "fixed precision" int pseudo
 # counts)
-import sys
 import math
+import sys
+
 from onmt.constants import DefaultTokens
 
 OMIT = (DefaultTokens.UNK, DefaultTokens.BOS, DefaultTokens.EOS)
@@ -10,7 +11,7 @@ OMIT = (DefaultTokens.UNK, DefaultTokens.BOS, DefaultTokens.EOS)
 
 def convert(lines):
     for line in lines:
-        w, c = line.rstrip("\n").split(None, 1)
+        w, c = line.rstrip('\n').split(None, 1)
         if w in OMIT:
             continue
         c = math.exp(float(c)) * 1000000
@@ -18,6 +19,9 @@ def convert(lines):
         yield w, c
 
 
-if __name__ == "__main__":
-    for c, w in convert(sys.stdin):
-        print("{}\t{}".format(c, w))
+if __name__ == '__main__':
+    sp_vocab_fn = sys.argv[1]  # SP词典路径
+    vocab_fn = sys.argv[2]  # 输出词典路径
+    with open(sp_vocab_fn, encoding="utf-8") as f, open(vocab_fn, "w", encoding="utf-8") as out:
+        for c, w in convert(f):
+            out.write('{}\t{}\n'.format(c, w))
