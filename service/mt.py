3652c986043e958f422dbf34e75e4ee1526f74fe
Xiaofeng Liu <liuxf@hust.edu.cn>
2024-05-17 11:03:40 +0800

    MT: 添加段落切分；增加段落翻译接口；增加Translators。

--- service/mt.py
+++ service/mt.py
@@ -1,6 +1,9 @@
+import threading
+
 from onmt.inference_engine import InferenceEnginePY
 from onmt.utils.parse import ArgumentParser
 import onmt.opts as opts
+from service.sentence_splitter import paragraph_tokenizer, paragraph_detokenizer
 
 
 class Translator:
@@ -14,11 +17,32 @@ class Translator:
 
         self.engine = InferenceEnginePY(self.opt)
         self.lang_pair = lang_pair
+        self.from_lang, self.to_lang = self.lang_pair.split("-")
 
     def translate_list(self, texts):
         scores, preds = self.engine.infer_list(texts)
 
         return [p[0] for p in preds]
+
+    def translate_paragraph(self, texts):
+        """Translate text paragraphs
+
+        the text will be segmented into paragraphs, and then paragraph segmented into sentences.
+        the format of text will be remained.
+
+        Args:
+            texts: text to be translated
+
+        Returns:
+             translated text with paragraphs
+        """
+        source_sents, breaks = paragraph_tokenizer(texts, self.from_lang)
+
+        translations = self.translate_list(source_sents)
+
+        translation = paragraph_detokenizer(translations, breaks)
+
+        return translation
 
     def directions(self):
         return [self.lang_pair]
@@ -27,6 +51,19 @@ class Translator:
         parser = ArgumentParser()
         opts.translate_opts(parser)
         return parser
+
+
+class Translators(object):
+
+    def __init__(self):
+        self.translators= {}
+
+    def get_translator(self, source_lang, target_lang, debug=False):
+        lang_pair = source_lang + "-" + target_lang
+        return self.translators[lang_pair]
+
+
+translator_factory = Translators()
 
 
 if __name__ == "__main__":
---
+++ service/sentence_splitter.py
@@ -0,0 +1,109 @@
+import pysbd
+from sentence_splitter import split_text_into_sentences
+from indicnlp.tokenize.sentence_tokenize import sentence_split
+
+tokenizers = {}
+
+
+
+def split_sentences(text, lang="en"):
+    """Segment paragraph into sentences
+
+    Args:
+        text: paragraph string
+        lang: language code string
+
+    Returns:
+        list of sentences
+    """
+    languages_splitter = ["ca", "cs", "da", "de", "el", "en", "es", "fi", "fr", "hu", "is", "it",
+                          "lt", "lv", "nl", "no", "pl", "pt", "ro", "ru", "sk", "sl", "sv", "tr"]
+    languages_indic = ["as", "bn", "gu", "hi", "kK", "kn", "ml", "mr", "ne", "or", "pa", "sa",
+                       "sd", "si", "ta", "te"]
+    languages_pysbd = ["en", "hi", "mr", "zh", "es", "am", "ar", "hy", "bg", "ur", "ru", "pl",
+                       "fa", "nl", "da", "fr", "my", "el", "it", "ja", "de", "kk", "sk"]
+
+    languages = languages_splitter + languages_indic + languages_pysbd
+    lang = lang if lang in languages else "en"
+
+    text = text.strip()
+
+    if lang in languages_pysbd:
+        segmenter = pysbd.Segmenter(language=lang, clean=True)
+        sentences = segmenter.segment(text)
+    elif lang in languages_splitter:
+        sentences = split_text_into_sentences(text, lang)
+    elif lang in languages_indic:
+        sentences = sentence_split(text, lang)
+
+    return sentences
+
+
+def paragraph_tokenizer(text, lang="en"):
+    """Replace sentences with their indexes, and store indexes of newlines
+    Args:
+        text (str): Text to be indexed
+
+    Returns:
+        sentences (list): List of sentences
+        breaks (list): List of indexes of sentences and newlines
+    """
+    text = text.strip()
+    paragraphs = text.splitlines(True)
+
+    breaks = []
+    sentences = []
+
+    for paragraph in paragraphs:
+        if paragraph == "\n":
+            breaks.append("\n")
+        else:
+            paragraph_sentences = split_sentences(paragraph, lang)
+
+            breaks.extend(
+                list(range(len(sentences), +len(sentences) + len(paragraph_sentences)))
+            )
+            breaks.append("\n")
+            sentences.extend(paragraph_sentences)
+
+    # Remove the last newline
+    breaks = breaks[:-1]
+
+    return sentences, breaks
+
+
+def paragraph_detokenizer(sentences, breaks):
+    """Restore original paragraph format from indexes of sentences and newlines
+
+    Args:
+        sentences (list): List of sentences
+        breaks (list): List of indexes of sentences and newlines
+
+    Returns:
+        text (str): Text with original format
+    """
+    output = []
+
+    for br in breaks:
+        if br == "\n":
+            output.append("\n")
+        else:
+            output.append(sentences[br] + " ")
+
+    text = "".join(output)
+    return text
+
+
+def may_combine_paragraph(text):
+    paragraphs = text.split("\n")
+    txt = ""
+    i = 0
+    while i < len(paragraphs):
+        p = paragraphs[i].strip()
+        i += 1
+        if len(p) == 0:  # blank paragraph
+            txt = txt + "\r\n\r\n"
+        else:
+            txt = txt + " " + p
+
+    return txt
