6feadfc6e845d51fc1f6d1653f8000010d593a68
liuxf_hust <hlp_ai@sina.com>
2024-05-23 20:58:49 +0800

    OCR: 添加英文识别例子数据集。

--- ocr/easyocr/trainer/crnn/dataset.py
+++ ocr/easyocr/trainer/crnn/dataset.py
@@ -273,14 +273,14 @@ class AlignCollate(object):
         return image_tensors, labels
 
 
-def tensor2im(image_tensor, imtype=np.uint8):
-    image_numpy = image_tensor.cpu().float().numpy()
-    if image_numpy.shape[0] == 1:
-        image_numpy = np.tile(image_numpy, (3, 1, 1))
-    image_numpy = (np.transpose(image_numpy, (1, 2, 0)) + 1) / 2.0 * 255.0
-    return image_numpy.astype(imtype)
-
-
-def save_image(image_numpy, image_path):
-    image_pil = Image.fromarray(image_numpy)
-    image_pil.save(image_path)
+# def tensor2im(image_tensor, imtype=np.uint8):
+#     image_numpy = image_tensor.cpu().float().numpy()
+#     if image_numpy.shape[0] == 1:
+#         image_numpy = np.tile(image_numpy, (3, 1, 1))
+#     image_numpy = (np.transpose(image_numpy, (1, 2, 0)) + 1) / 2.0 * 255.0
+#     return image_numpy.astype(imtype)
+#
+#
+# def save_image(image_numpy, image_path):
+#     image_pil = Image.fromarray(image_numpy)
+#     image_pil.save(image_path)
