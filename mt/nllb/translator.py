592a20c52676b08675d15b89b1592e860210a9fd
Xiaofeng Liu <liuxf@hust.edu.cn>
2024-05-24 15:40:12 +0800

    MT: 删除NLLB代码。

--- mt/nllb/translator.py
+++
@@ -1,9 +0,0 @@
-from onmt.translate import TranslationServer
-
-config_file = "./nllb.conf.json"
-translation_server = TranslationServer()
-translation_server.start(config_file)
-
-inputs = [{"id": 100,
-         "src": "This is a test."}]
-trans, scores, n_best, _, aligns = translation_server.run(inputs)
