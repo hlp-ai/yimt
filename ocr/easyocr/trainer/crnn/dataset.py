28731f7c3c6d8b32602f8110a23947bc2da47ede
Xiaofeng Liu <liuxf@hust.edu.cn>
2024-05-24 11:17:05 +0800

    OCR: 精简例子数据集；添加数据集使用例子代码；更新训练配置文件；

--- ocr/easyocr/trainer/crnn/dataset.py
+++ ocr/easyocr/trainer/crnn/dataset.py
@@ -206,26 +206,6 @@ class ResizeNormalize(object):
         return img
 
 
-# class NormalizePAD(object):
-#
-#     def __init__(self, max_size, PAD_type='right'):
-#         self.toTensor = transforms.ToTensor()
-#         self.max_size = max_size
-#         self.max_width_half = math.floor(max_size[2] / 2)
-#         self.PAD_type = PAD_type
-#
-#     def __call__(self, img):
-#         img = self.toTensor(img)
-#         img.sub_(0.5).div_(0.5)
-#         c, h, w = img.size()
-#         Pad_img = torch.FloatTensor(*self.max_size).fill_(0)
-#         Pad_img[:, :, :w] = img  # right pad
-#         if self.max_size[2] != w:  # add border Pad
-#             Pad_img[:, :, w:] = img[:, :, w - 1].unsqueeze(2).expand(c, h, self.max_size[2] - w)
-#
-#         return Pad_img
-
-
 class AlignCollate(object):
 
     def __init__(self, imgH=32, imgW=100, keep_ratio_with_pad=False, contrast_adjust = 0.):
@@ -273,14 +253,13 @@ class AlignCollate(object):
         return image_tensors, labels
 
 
-# def tensor2im(image_tensor, imtype=np.uint8):
-#     image_numpy = image_tensor.cpu().float().numpy()
-#     if image_numpy.shape[0] == 1:
-#         image_numpy = np.tile(image_numpy, (3, 1, 1))
-#     image_numpy = (np.transpose(image_numpy, (1, 2, 0)) + 1) / 2.0 * 255.0
-#     return image_numpy.astype(imtype)
-#
-#
-# def save_image(image_numpy, image_path):
-#     image_pil = Image.fromarray(image_numpy)
-#     image_pil.save(image_path)
+if __name__ == "__main__":
+    from easyocr.trainer.crnn.train_main import get_config
+
+    opt = get_config("config_files/en_filtered_config.yaml")
+
+    train_dataset = Batch_Balanced_Dataset(opt)
+    image_tensors, labels = train_dataset.get_batch()
+
+    print(image_tensors)
+    print(labels)
