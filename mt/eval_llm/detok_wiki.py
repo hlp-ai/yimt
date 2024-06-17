42365f9a6fc34be59c16c7c96b05608c55e031dd
Xiaofeng Liu <liuxf@hust.edu.cn>
2024-06-17 19:25:16 +0800

    MT: 添加wiki去词条化代码。

---
+++ mt/eval_llm/detok_wiki.py
@@ -0,0 +1,47 @@
+import re
+import sys
+
+
+def wikitext_detokenizer(line):
+    string = line
+    # contractions
+    string = string.replace("s '", "s'")
+    string = re.sub(r"/' [0-9]/", r"/'[0-9]/", string)
+    # number separators
+    string = string.replace(" @-@ ", "-")
+    string = string.replace(" @,@ ", ",")
+    string = string.replace(" @.@ ", ".")
+    # punctuation
+    string = string.replace(" : ", ": ")
+    string = string.replace(" ; ", "; ")
+    string = string.replace(" . ", ". ")
+    string = string.replace(" ! ", "! ")
+    string = string.replace(" ? ", "? ")
+    string = string.replace(" , ", ", ")
+    # double brackets
+    string = re.sub(r"\(\s*([^\)]*?)\s*\)", r"(\1)", string)
+    string = re.sub(r"\[\s*([^\]]*?)\s*\]", r"[\1]", string)
+    string = re.sub(r"{\s*([^}]*?)\s*}", r"{\1}", string)
+    string = re.sub(r"\"\s*([^\"]*?)\s*\"", r'"\1"', string)
+    string = re.sub(r"'\s*([^']*?)\s*'", r"'\1'", string)
+    # miscellaneous
+    string = string.replace("= = = =", "====")
+    string = string.replace("= = =", "===")
+    string = string.replace("= =", "==")
+    string = string.replace(" " + chr(176) + " ", chr(176))
+    string = string.replace(" \n", "\n")
+    string = string.replace("\n ", "\n")
+    string = string.replace(" N ", " 1 ")
+    string = string.replace(" 's", "'s")
+    return string
+
+
+if __name__ == "__main__":
+    corpus_file = sys.argv[1]
+
+    output = corpus_file + ".detok"
+
+    with open(corpus_file, encoding="utf-8") as f, open(output, "w", encoding="utf-8") as out:
+        for line in f:
+            line = wikitext_detokenizer(line)
+            out.write(line)
