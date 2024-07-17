882ee9489b36aa5d543d80ceecdfd5428086ae04
Xiaofeng Liu <liuxf@hust.edu.cn>
2024-07-17 10:56:07 +0800

    MT: ElementWise移到embeddings.py，不再外部使用.

--- mt/onmt/modules/__init__.py
+++ mt/onmt/modules/__init__.py
@@ -1,12 +1,10 @@
 """  Attention and normalization modules  """
-from onmt.modules.util_class import Elementwise
 from onmt.modules.multi_headed_attn import MultiHeadedAttention
 from onmt.modules.embeddings import Embeddings, PositionalEncoding
 from onmt.modules.rmsnorm import RMSNorm
 
 
 __all__ = [
-    "Elementwise",
     "MultiHeadedAttention",
     "Embeddings",
     "PositionalEncoding",
--- mt/onmt/modules/embeddings.py
+++ mt/onmt/modules/embeddings.py
@@ -5,12 +5,38 @@ import torch
 import torch.nn as nn
 from torch.nn.utils import skip_init
 
-from onmt.modules.util_class import Elementwise
 from onmt.utils.logging import logger
 
 
 class SequenceTooLongError(Exception):
     pass
+
+
+# At the moment this class is only used by embeddings.Embeddings look-up tables
+class Elementwise(nn.ModuleList):
+    """
+    A simple network container.
+    Parameters are a list of modules.
+    emb is a 3d Tensor whose last dimension is the same length
+    as the list.
+    emb_out is the result of applying modules to emb elementwise.
+    An optional merge parameter allows the emb_out to be reduced to a
+    single Tensor.
+    """
+
+    def __init__(self, merge=None, *args):
+        super(Elementwise, self).__init__(*args)
+
+    def forward(self, emb):
+        emb_ = [feat.squeeze(2) for feat in emb.split(1, dim=2)]
+        emb_out = []
+
+        # for some reason list comprehension is slower in this scenario
+        for f, x in zip(self, emb_):
+            emb_out.append(f(x))
+
+        return emb_out[0]
+
 
 
 class PositionalEncoding(nn.Module):
