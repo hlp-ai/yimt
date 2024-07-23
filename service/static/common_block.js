e9906d7d7ef1193a9a1f865222b49ef8077789cc
Xiaofeng Liu <liuxf@hust.edu.cn>
2024-07-23 20:14:50 +0800

    MT: 修改文本翻译页面翻译设置块.

--- service/static/common_block.js
+++ service/static/common_block.js
@@ -1,32 +1,14 @@
 
 
-function set_setting(){
-    document.getElementById("url_setting_block").innerHTML = `
-        <label id="translation_setting_label">翻译设置</label>
-        <div id="translation_setting_block1">
-            <label id="server_label">服务器：</label>
-            <input type="text" class="url" id="url">
-        </div>
-        <div id="translation_setting_block2">
-            <label id="api_key_label">API KEY：</label>
-            <input type="text" class="api_key" id="api_key" placeholder="api_key">
-        </div>
-        <input type="button" class="url_setting_hide" value="关闭" onclick="url_setting_hide_func()">
-        <input type="button" class="url_button" id="url_button" value="设置" onclick="url_resetting_func()">
-    `;
-
-    document.getElementById("settings").addEventListener("click", url_setting_func);
-}
-
-function url_setting_func()
+function display_setting_func()
 {
-    document.getElementById("url_setting_block").style.visibility='visible';
-    document.getElementById("mask").style.display='block';
-    document.getElementById("url").value=END_POINT;
+    document.getElementById("settings_block").style.visibility='visible';
+    document.getElementById("settings_mask").style.display='block';
+    document.getElementById("server_url").value=END_POINT;
     document.getElementById("api_key").value=API_KEY;
 }
 
-function url_resetting_func()
+function reset_setting_func()
 {
     END_POINT = document.getElementById("url").value;
     window.localStorage.setItem("server", END_POINT);
@@ -38,8 +20,8 @@ function url_resetting_func()
     window.localStorage.setItem("key",API_KEY);
 }
 
-function url_setting_hide_func()
+function close_setting_func()
 {
-    document.getElementById("url_setting_block").style.visibility='hidden';
-    document.getElementById("mask").style.display='none'
+    document.getElementById("settings_block").style.visibility='hidden';
+    document.getElementById("settings_mask").style.display='none'
 }
