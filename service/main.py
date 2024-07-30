95a73a0affe0a05c8b3e780d2e7e49e89be27c18
Xiaofeng Liu <liuxf@hust.edu.cn>
2024-07-30 16:33:53 +0800

    MT: service.main使用中文参数描述。

--- service/main.py
+++ service/main.py
@@ -18,45 +18,45 @@ DEFAULT_ARGS = {
 
 def get_args():
     parser = argparse.ArgumentParser(
-        description="YiMT Translation API"
+        description="YiMT翻译服务器"
     )
     parser.add_argument(
-        "--host", type=str, help="Hostname (%(default)s)", default=DEFAULT_ARGS['HOST']
+        "--host", type=str, help="翻译服务运行主机(%(default)s)", default=DEFAULT_ARGS['HOST']
     )
-    parser.add_argument("--port", type=int, help="Port (%(default)s)", default=DEFAULT_ARGS['PORT'])
+    parser.add_argument("--port", type=int, help="翻译服务端口(%(default)s)", default=DEFAULT_ARGS['PORT'])
     parser.add_argument(
         "--char-limit",
         default=DEFAULT_ARGS['CHAR_LIMIT'],
         type=int,
         metavar="<number of characters>",
-        help="Set character limit (%(default)s)",
+        help="翻译请求最大字符长度(%(default)s)",
     )
     parser.add_argument(
         "--req-limit",
         default=DEFAULT_ARGS['REQ_LIMIT'],
         type=int,
         metavar="<number>",
-        help="Set the default maximum number of requests per minute per client (%(default)s)",
+        help="每个客户每分钟最大翻译请求数(%(default)s)",
     )
     parser.add_argument(
         "--daily-req-limit",
         default=DEFAULT_ARGS['DAILY_REQ_LIMIT'],
         type=int,
         metavar="<number>",
-        help="Set the default maximum number of requests per day per client, in addition to req-limit. (%(default)s)",
+        help="每个客户每天最大翻译请求数(%(default)s)",
     )
     parser.add_argument(
         "--api-keys",
         default=DEFAULT_ARGS['API_KEYS'],
         action="store_true",
-        help="Enable API keys database for per-user rate limits lookup",
+        help="激活用户API数据库配置信息",
     )
     parser.add_argument(
-        "--disable-web-ui", default=DEFAULT_ARGS['DISABLE_WEB_UI'], action="store_true", help="Disable web ui"
+        "--disable-web-ui", default=DEFAULT_ARGS['DISABLE_WEB_UI'], action="store_true", help="关闭翻译Web界面"
     )
     parser.add_argument(
         "--disable-files-translation", default=DEFAULT_ARGS['DISABLE_FILES_TRANSLATION'], action="store_true",
-        help="Disable files translation"
+        help="关闭文档翻译"
     )
 
     return parser.parse_args()
