b3f7aa31ad135254b4be74d4b9d9b0ebadfe45be
Xiaofeng Liu <liuxf@hust.edu.cn>
2024-08-15 10:15:10 +0800

    MT: 优化extract_vocabulary.py。

--- mt/tools/extract_vocabulary.py
+++ mt/tools/extract_vocabulary.py
@@ -4,30 +4,23 @@ import argparse
 
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-model", type=str, required=True)
-    parser.add_argument("-out_file", type=str, required=True)
-    parser.add_argument(
-        "-side",
-        choices=["src", "tgt"],
-        help="""Specifies
-                               'src' or 'tgt' side for 'vocab' file_type.""",
-    )
+    parser = argparse.ArgumentParser("导出模型词典")
+    parser.add_argument("-model", type=str, required=True, help="模型路径")
+    parser.add_argument("-out_file", type=str, required=True, help="保存词典路径")
+    parser.add_argument("-side", choices=["src", "tgt"], help="导出哪个方向的词典")
 
     opt = parser.parse_args()
 
     if opt.side not in ["src", "tgt"]:
-        raise ValueError(
-            "If using -file_type='vocab', specifies "
-            "'src' or 'tgt' argument for -side."
-        )
+        raise ValueError("词典方向为src或tgt")
+
     import torch
 
-    print("Reading input file...")
+    print("读入模型文件...")
     model = torch.load(opt.model, map_location=torch.device("cpu"))
     voc = model["vocab"][opt.side]
 
-    print("Writing vocabulary file...")
+    print("写出词典到文件...")
     with open(opt.out_file, "wb") as f:
         for w in voc:
             f.write("{0}\n".format(w).encode("utf-8"))
