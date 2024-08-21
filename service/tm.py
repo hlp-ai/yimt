ab54eb0b4f6678fc763155f16ecb07284eafad8a
Xiaofeng Liu <liuxf@hust.edu.cn>
2024-08-21 20:16:18 +0800

    MT: 改进翻译记忆。

--- service/tm.py
+++ service/tm.py
@@ -1,22 +1,61 @@
 import os
 import time
 from time import strftime
+import re
 
 
-class TranslationRecord:
+class TMRecord:
 
-    def __init__(self):
-        self.lang_pair = None
-        self.original = None
-        self.translation = None
+    def __init__(self, lang_pair=None, source=None, target=None):
+        self.lang_pair = lang_pair
+        self.source = source
+        self.target = target
+
+        if self.lang_pair:
+            self.sl, self.tl = self.lang_pair.split("-")
+
+    def __str__(self):
+        return BasicTMSaver.RECORD_PATTERN.format(self.lang_pair, self.source, self.target)
+
+    def __hash__(self):
+        return hash(self.lang_pair + self.source + self.target)
+
+    def __eq__(self, other):
+        return self.lang_pair==other.lang_pair and self.source==other.source and self.target==other.target
+
+
+class TMLoader:
+
+    def tm_list(self):
+        pass
+
+
+class BasicTMLoader(TMLoader):
+
+    pattern = re.compile("<lang-pair>([^<]+?)</lang-pair>\n<source>([^<]+?)</source>\n<target>([^<]+?)</target>",
+                         re.DOTALL)
+
+    def __init__(self, tm_file):
+        self.tm_records = []
+        with open(tm_file, encoding="utf-8") as f:
+            text = f.read()
+
+            matches = self.pattern.findall(text)
+            for match in matches:
+                self.tm_records.append(TMRecord(match[0].strip(),
+                                                match[1].strip(),
+                                                match[2].strip()))
+
+    def tm_list(self):
+        return self.tm_records
 
 
 class TMSaver:
 
-    def save(self, tr):
+    def save(self, tm):
         pass
 
-    def save_info(self, lang_pair, original, translation):
+    def save_info(self, lang_pair, source, target):
         pass
 
     def close(self):
@@ -27,7 +66,7 @@ class TMSaver:
 
 
 class BasicTMSaver(TMSaver):
-    RECORD_PATTERN = "<lang-pair>\n{}\n</lang-pair>\n<original>\n{}\n</original>\n<translation>\n{}\n</translation>\n\n"
+    RECORD_PATTERN = "<lang-pair>\n{}\n</lang-pair>\n<source>\n{}\n</source>\n<target>\n{}\n</target>\n\n"
 
     def __init__(self, tm_dir=None):
         if tm_dir is None:
@@ -37,18 +76,18 @@ class BasicTMSaver(TMSaver):
         self.fn_prefix = os.path.join(tm_dir, strftime("%Y%m%d.tm", time.localtime()))
         self.tm_f = open(self.fn_prefix, "a", encoding="utf-8")
 
-    def save(self, tr):
-        record = self.RECORD_PATTERN.format(tr.lang_pair, tr.original, tr.translation)
+    def save(self, tm):
+        record = self.RECORD_PATTERN.format(tm.lang_pair, tm.source, tm.target)
         self.tm_f.write(record)
 
-    def save_info(self, lang_pair, original, translation):
-        if isinstance(original, list):
-            assert len(original) == len(translation)
-            for i in range(len(original)):
-                record = self.RECORD_PATTERN.format(lang_pair, original[i], translation[i])
+    def save_info(self, lang_pair, source, target):
+        if isinstance(source, list):
+            assert len(source) == len(target)
+            for i in range(len(source)):
+                record = self.RECORD_PATTERN.format(lang_pair, source[i], target[i])
                 self.tm_f.write(record)
         else:
-            record = self.RECORD_PATTERN.format(lang_pair, original, translation)
+            record = self.RECORD_PATTERN.format(lang_pair, source, target)
             self.tm_f.write(record)
 
     def close(self):
@@ -71,12 +110,21 @@ def get_tm_saver():
 
 
 if __name__ == "__main__":
-    tm = TranslationRecord()
+    tm = TMRecord()
     tm.lang_pair = "en-zh"
-    tm.original = "This is a book."
-    tm.translation = "这是一本书。"
+    tm.source = "This is a book."
+    tm.target = "这是一本书。"
 
     saver = get_tm_saver()
     saver.save(tm)
     saver.save(tm)
     saver.close()
+
+    tm_loader = BasicTMLoader("./tm/20240821.tm")
+    unique_tm = set()
+    print(len(tm_loader.tm_list()))
+    for r in tm_loader.tm_list():
+        print(r)
+        unique_tm.add(r)
+
+    print(len(unique_tm))
