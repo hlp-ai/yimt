7dbdd037462845c0c9abe37d61c64903dbe06b68
Xiaofeng Liu <liuxf@hust.edu.cn>
2024-09-24 15:33:39 +0800

    MT: 增加AD类型；重构ADDB；增加从文件读取AD。

--- service/ad_db.py
+++ service/ad_db.py
@@ -2,16 +2,29 @@ import random
 
 from service.utils import get_logger
 
+from collections import namedtuple
+
+AD = namedtuple('AD', ['id', 'type', 'content', 'url'])
+
 
 class ADDB:
 
+    def __init__(self):
+        # 广告展示记录日志
+        self.logger_ad = get_logger(log_filename="ad.log", name="AD")
+
     def get_ad(self, type):
         """获得给定类型的广告"""
-        pass
+        ads = self.all_ad[type]
+        n = len(ads)
+        idx = random.randint(0, n - 1)
+
+        return ads[idx]
 
     def log_ad(self, platform, ad_id, where=None, action="P"):
         """记录广告播放情况"""
-        pass
+        where = where + ": " if where else "";
+        self.logger_ad.info(platform + " " + where + ad_id + " " + action)
 
 
 class ADList(ADDB):
@@ -24,20 +37,24 @@ class ADList(ADDB):
                                 ("AD-20221023", "text", "广告内容2广告内容", "https://www.hust.edu.cn")]
                        }
 
-        # 广告展示记录日志
-        self.logger_ad = get_logger(log_filename="ad.log", name="AD")
+class ADFile(ADDB):
 
-    def get_ad(self, type):
-        ads = self.all_ad[type]
-        n = len(ads)
-        idx = random.randint(0, n - 1)
+    def __init__(self, ad_file):
+        super().__init__()
 
-        return ads[idx]
+        import csv
 
-    def log_ad(self, platform, ad_id, where=None, action="P"):
-        where = where+": " if where else "";
-        self.logger_ad.info(platform + " " + where + ad_id + " " + action)
+        with open(ad_file, 'r', encoding="utf-8") as file:
+            reader = csv.reader(file)
+            rows = list(reader)[1:]
 
+        self.all_ad = {}
+
+        for r in rows:
+            ad = AD(*r)
+            if ad.type not in self.all_ad:
+                self.all_ad[ad.type] = []
+            self.all_ad[ad.type].append(ad)
 
 ad_db = None
 
@@ -49,3 +66,11 @@ def get_addb():
 
     ad_db = ADList()
     return ad_db
+
+
+if __name__ == "__main__":
+    ads = ADFile("./ads.csv")
+
+    ad1 = ads.get_ad("text")
+    print(ad1)
+    print(ad1[0], ad1.id)
