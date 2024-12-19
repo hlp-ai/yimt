5b5d8c8924619bd376871670a411904daff0cb5f
Xiaofeng Liu <liuxf@hust.edu.cn>
2024-12-19 15:21:08 +0800

    TTS: 增加注释。

--- tts/vits/preprocess.py
+++ tts/vits/preprocess.py
@@ -1,3 +1,4 @@
+"""对文件中文本进行clean"""
 import argparse
 from vits import text
 from vits.utils import load_filepaths_and_text
@@ -14,12 +15,12 @@ if __name__ == '__main__':
 
     for filelist in args.filelists:
         print("START:", filelist)
-        filepaths_and_text = load_filepaths_and_text(filelist)
+        filepaths_and_text = load_filepaths_and_text(filelist)  # 每行格式：音频文件路径|文本
         for i in range(len(filepaths_and_text)):
             original_text = filepaths_and_text[i][args.text_index]
-            cleaned_text = text._clean_text(original_text, args.text_cleaners)
+            cleaned_text = text._clean_text(original_text, args.text_cleaners)  # 清理文本
             filepaths_and_text[i][args.text_index] = cleaned_text
 
-        new_filelist = filelist + "." + args.out_extension
+        new_filelist = filelist + "." + args.out_extension  # 清理后的文件
         with open(new_filelist, "w", encoding="utf-8") as f:
             f.writelines(["|".join(x) + "\n" for x in filepaths_and_text])
