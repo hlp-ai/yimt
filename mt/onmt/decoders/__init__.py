a147c181162f71bbe4898b79d8cbd2f7b79f1a64
Xiaofeng Liu <liuxf@hust.edu.cn>
2025-02-11 10:32:28 +0800

    MT: 删除无意义的encoder和decoder包代码。

--- mt/onmt/decoders/__init__.py
+++ mt/onmt/decoders/__init__.py
@@ -1,28 +1,6 @@
 """Module defining decoders."""
-import os
-import importlib
-
-from onmt.decoders.decoder import DecoderBase
 from onmt.decoders.transformer import TransformerDecoder
 
 str2dec = {
     "transformer": TransformerDecoder,
 }
-
-__all__ = [
-    "TransformerDecoder",
-    "str2dec",
-]
-
-
-# Auto import python files in this directory
-decoder_dir = os.path.dirname(__file__)
-for file in os.listdir(decoder_dir):
-    path = os.path.join(decoder_dir, file)
-    if (
-        not file.startswith("_")
-        and not file.startswith(".")
-        and (file.endswith(".py") or os.path.isdir(path))
-    ):
-        file_name = file[: file.find(".py")] if file.endswith(".py") else file
-        module = importlib.import_module("onmt.decoders." + file_name)
--- mt/onmt/encoders/__init__.py
+++ mt/onmt/encoders/__init__.py
@@ -1,7 +1,4 @@
 """Module defining encoders."""
-import os
-import importlib
-from onmt.encoders.encoder import EncoderBase
 from onmt.encoders.transformer import TransformerEncoder
 
 
@@ -9,21 +6,3 @@ str2enc = {
     "transformer": TransformerEncoder,
 }
 
-__all__ = [
-    "EncoderBase",
-    "TransformerEncoder",
-    "str2enc",
-]
-
-
-# Auto import python files in this directory
-encoder_dir = os.path.dirname(__file__)
-for file in os.listdir(encoder_dir):
-    path = os.path.join(encoder_dir, file)
-    if (
-        not file.startswith("_")
-        and not file.startswith(".")
-        and (file.endswith(".py") or os.path.isdir(path))
-    ):
-        file_name = file[: file.find(".py")] if file.endswith(".py") else file
-        module = importlib.import_module("onmt.encoders." + file_name)
